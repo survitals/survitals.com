b07060f7db0caa24dfaffafdf0a124710c93202c
Ryan Van Etten <ryanve@gmail.com>
2013-08-15 16:36:30 -0400

    Correct paths.

--- _markup/configure.php
+++ _markup/configure.php
@@ -6,5 +6,5 @@ Loci::option('uri:repo') and Loci::on('normalize', function() {
     $repo = Loci::option('uri:repo');
     $ctxt = Loci::context();
     $hier = \strtok($_SERVER['REQUEST_URI'], '?#');
-    $ctxt->data('url.tree', Path::join($repo, 'tree/master', $hier));
+    $ctxt->data('url.tree', Path::join($repo, 'tree/master', Loci::option(''), $hier));
 });
