247ca6dea255e398ad168054c2a7a3edb29e410c
Ryan Van Etten <ryanve@gmail.com>
2014-01-13 18:04:25 -0500

    More 2-space indents and tag omissions.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ namespace chromosome;
 use \slash\Path;
 require_once __DIR__ . '/_package/chromosome/bootstrap.php';
 echo loci(\array_merge(json_decode('{
-    "title": "Survitals: Safety, Survival, and Preparedness",
-    "type": "home plural",
-    "author": "Ryan Van Etten",
-    "description": "Learn about modern survival, safety, emergency preparedness, situation awareness, and rescue."
+  "title": "Survitals: Safety, Survival, and Preparedness",
+  "type": "home plural",
+  "author": "Ryan Van Etten",
+  "description": "Learn about modern survival, safety, emergency preparedness, situation awareness, and rescue."
 }', true), ['list' => Path::getJson(__DIR__ . '/list.json')]))->render();
