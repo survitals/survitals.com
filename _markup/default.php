2bfb257171f85d91c034e7d4871a6bd98c95d48e
Ryan Van Etten <ryanve@gmail.com>
2014-01-13 16:26:01 -0500

    Omit optional HTML tags.

--- _markup/default.php
+++ _markup/default.php
@@ -10,56 +10,49 @@ use \airve\Phat;
   $hier and $list[] = \rtrim('/' . Phat::esc($hier), '/');
   $list[] = 'base-' . \basename($_SERVER['REQUEST_URI']);
   $list[] = 'view-' . Path::filename(__FILE__);
-  return ['lang' => 'en-US', 'class' => $list, 'itemscope'];
-}) : '<html lang="en-US" itemscope>'; ?>
+  return ['id' => 'start', 'lang' => 'en-US', 'class' => $list, 'itemscope'];
+}) : '<html id="start" lang="en-US" itemscope>'; ?>
 
-<head>
-  <meta charset='utf-8'>
-  <title>{{titlebar}}</title>
-  <meta name='viewport' content='width=device-width,initial-scale=1.0'>
-  <?php echo Loci::meta('author', loci()->data('author:name') ?: loci()->data('author')); ?>
-  <?php echo Loci::meta('description', loci()->data('description')); ?>
-  
-  <link rel=stylesheet href="/css/base.<?php echo date('YW'); ?>.css">
-  <link rel=stylesheet href="/css/main.<?php echo date('YmdH'); ?>.css" media="screen,projection">
-  <?php echo \rtrim(\call_user_func(function($ctxt, $tab = '    ', $markup = '') {
-    if ($value = $ctxt->data('url'))
-      $markup .= "<link rel=canonical href='$value'>\n$tab";
-    if ($value = $ctxt->data('thumb'))
-      $markup .= "<link rel=image_src href='$value'>\n$tab<meta property=og:image content='$value'>\n$tab";
-    if (trim($_SERVER['REQUEST_URI'], '/'))
-      $markup .= "<link rel='prerender prefetch' href=/>\n$tab"; 
-    return $markup;
-  }, loci())) . "\n"; ?>
-</head>
-<body id="start">
+<meta charset=utf-8>
+<title>{{titlebar}}</title>
+<meta name='viewport' content='width=device-width,initial-scale=1.0'>
+<?php echo Loci::meta('author', loci()->data('author:name') ?: loci()->data('author')); ?>
+<?php echo Loci::meta('description', loci()->data('description')); ?>
 
-<?php include 'site-header.php'; ?>
+<link rel=stylesheet href="/css/base.<?php echo date('YW'); ?>.css">
+<link rel=stylesheet href="/css/main.<?php echo date('YmdH'); ?>.css" media="screen,projection">
 
-  <main id="main" role="main">
-    <div class="container">
+<?php echo \call_user_func(function($ctx, $a = []) {
+  if (trim($_SERVER['REQUEST_URI'], '/')) $a[] = "<link rel='prerender prefetch' href=/>"; 
+  if ($v = $ctx->data('url')) $a[] = "<link rel=canonical href='$v'>";
+  if ($v = $ctx->data('thumb')) \array_push($a, "<link rel=image_src href='$v'>", "<meta property=og:image content='$v'>");
+  return \trim(\implode("\n", $a)) . "\n";
+}, loci());
 
-    <?php \call_user_func(function() {
-      $ctxt = Loci::context();
-      $list = $ctxt->data('list');
-      $file = Loci::option('basename:html');
-      if (!\is_array($list)) { 
-        include ('article.php');
-      } elseif ($from = Loci::option('path:items')) {
-        foreach ($list as $path) {
-          $post = loci(Path::join($from, $path));
-          Path::contains($post->data('type'), 'draft') or $post->render_e('article-link');
-        }
+include 'site-header.php'; ?>
+
+<main id="main" role="main">
+  <div class="container">
+
+  <?php \call_user_func(function() {
+    $ctx = Loci::context();
+    $list = $ctx->data('list');
+    $file = Loci::option('basename:html');
+    if (!\is_array($list)) { 
+      include 'article.php';
+    } elseif ($from = Loci::option('path:items')) {
+      foreach ($list as $path) {
+        $post = loci(Path::join($from, $path));
+        Path::contains($post->data('type'), 'draft') or $post->render_e('article-link');
       }
-    }); ?>
+    }
+  }); ?>
 
-    </div>
-  </main>
+  </div>
+</main>
 
 <?php include 'site-footer.php'; ?>
 
-  <a id="end"></a>
+<a id="end"></a>
 
-<?php include 'site-analytics.php'; ?>
-</body>
-</html>
+<?php include 'site-analytics.php';
